072262e551810521eeb2b5827ae4b7552d139a05
andreww <andreww@gchq.gov.uk>
2018-09-18 16:57:40 +0100

    Created REST Service

--- src/main/java/demo/models/Employee.java
+++ src/main/java/demo/models/Employee.java
@@ -1,6 +1,7 @@
 package demo.models;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +9,7 @@ import javax.persistence.Id;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Employee {
 
     private @Id @GeneratedValue Long id;
